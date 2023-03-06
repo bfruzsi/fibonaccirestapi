a8d4c5a9d951b7b24b19b8f30165d75b6126570f
bfruzsi <bfruzsina@stud.uni-obuda.hu>
2023-03-06 12:23:28 +0100

    Test case created

--- src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
+++ src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
@@ -1,8 +1,11 @@
 package hu.obuda.devops.fibonaccirestapi.service
 
+import hu.obuda.devops.fibonaccirestapi.controller.FibonacciController
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FibonacciServiceTest {
@@ -19,4 +22,21 @@ class FibonacciServiceTest {
     }
 
     // TODO - Test with greater numbers and test edge cases
+
+    //@Test
+    //fun shouldReturnBadRequestWhenGraterThat46(){
+        //val result: Int = underTest.fibonacci(48)
+
+        //Assertions.assertThrowsExactly(FibonacciController , ResponseStatusException(HttpStatus.BAD_REQUEST))
+    //}
+
+    @Test
+    fun shouldReturn2WhenCall3() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(3)
+        // then
+        Assertions.assertEquals(2, result)
+    }
 }
